c978e5a1de052bcfe5dd0d3ae6e37f283054b345
Luizfpereira <luizpereira.mec@outlook.com>
2024-10-17 19:06:22 -0300

    insert, search, traverse and prepend methods in linked list

--- lists/linked_list.py
+++ lists/linked_list.py
@@ -21,7 +21,6 @@ class LinkedList:
 
         return result
 
-
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -32,15 +31,68 @@ class LinkedList:
             self.tail = new_node
         self.length += 1
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp_node = self.head
+
+            for _ in range(index-1):
+                temp_node = temp_node.next
+            new_node.next = temp_node.next
+            temp_node.next = new_node
+
+            if new_node.next is None:
+                self.tail = new_node
+        self.length += 1
+
+        return True
+    
+    def traverse(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
+    def search(self, target):
+        current = self.head
+        index = 0
+
+        while current:
+            if current.value == target:
+                return index
+            current = current.next
+            index += 1
+        return -1
+            
 
 
-new_list = LinkedList()
-print(new_list.head, new_list.tail)
-print(new_list)
 
-new_list.append(10)
-new_list.append(20)
+# new_list = LinkedList()
+# print(new_list.head, new_list.tail)
+# print(new_list)
 
-print(new_list.head.value, new_list.tail.value)
-print(new_list)
+# new_list.append(10)
+# new_list.append(20)
+# new_list.insert(2, 50)
 
+# print(new_list.head.value, new_list.tail.value)
+# print(new_list.search(50))
--- lists/test_linked_list.py
+++ lists/test_linked_list.py
@@ -31,6 +31,96 @@ class TestLinkedList(unittest.TestCase):
             expected = '10 -> 20'
             self.assertEqual(expected, str(new_list))
 
+        def test_prepend_multiple_node(self):
+            new_list = LinkedList()
+            new_list.prepend(10)
+            new_list.prepend(20)
+            self.assertEqual(new_list.head.value, 20)
+            self.assertEqual(new_list.tail.value, 10)
+            self.assertEqual(new_list.length, 2)
+
+        def test_insert_empty_linked_list(self):
+            new_list = LinkedList()
+            new_list.insert(0, 50)
+            self.assertEqual(new_list.head.value, 50)
+            self.assertEqual(new_list.tail.value, 50)
+            self.assertEqual(new_list.length, 1)
+
+        def test_insert_beginning_linked_list(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            new_list.insert(0, 50)
+            self.assertEqual(new_list.head.value, 50)
+            self.assertEqual(new_list.tail.value, 30)
+            self.assertEqual(new_list.length, 4)
+        
+        def test_insert_end_linked_list(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            new_list.insert(3, 50)
+            self.assertEqual(new_list.head.value, 10)
+            self.assertEqual(new_list.tail.value, 50)
+            self.assertEqual(new_list.length, 4)
+
+        def test_insert_middle_linked_list(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            new_list.insert(2, 50)
+            expected = '10 -> 20 -> 50 -> 30'
+            self.assertEqual(expected, str(new_list))
+            self.assertEqual(new_list.head.value, 10)
+            self.assertEqual(new_list.tail.value, 30)
+            self.assertEqual(new_list.length, 4)
+
+        def test_insert_negative_index(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            res = new_list.insert(-1, 50)
+            self.assertEqual(new_list.head.value, 10)
+            self.assertEqual(new_list.tail.value, 30)
+            self.assertEqual(new_list.length, 3)
+            self.assertFalse(res)
+        
+        def test_insert_index_out_of_bound(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            res = new_list.insert(6, 50)
+            self.assertEqual(new_list.head.value, 10)
+            self.assertEqual(new_list.tail.value, 30)
+            self.assertEqual(new_list.length, 3)
+            self.assertFalse(res)
+
+        def search_empty_list(self):
+            new_list = LinkedList()
+            res = new_list.search(50)
+            self.assertEqual(res, -1)
+
+        def search_filled_list(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            res = new_list.search(30)
+            self.assertEqual(res, 2)
+
+        def search_non_existant(self):
+            new_list = LinkedList()
+            new_list.append(10)
+            new_list.append(20)
+            new_list.append(30)
+            res = new_list.search(50)
+            self.assertEqual(res, -1)
+
 
 if __name__ == '__main__':
     unittest.main()
